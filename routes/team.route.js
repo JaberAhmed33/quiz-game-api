5cd56b6681cf0fa1d72c3620f4ff49b45e810667
Jaber Ahmed <70724851+JaberAhmed33@users.noreply.github.com>
2024-06-14 17:57:46 +0300

    Update team.route.js

--- routes/team.route.js
+++ routes/team.route.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const { getTeams, addTeam, getTeam } = require("../controllers/team.controller");
+const { getTeams, addTeam, getTeam, editTeam } = require("../controllers/team.controller");
 
 const router = express.Router();
 
 router.get("/", getTeams);
 router.post("/addTeam", addTeam);
 router.get("/:id", getTeam);
+router.patch("/edit/:id", editTeam);
 
 module.exports = router;
